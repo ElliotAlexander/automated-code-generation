e560513db7e2d17c782f873878cf789c404e897e
Elliot Alexander <ep1e16@soton.ac.uk>
2020-03-03 17:24:10 +0000

    Fixed Question 1- seems to be working as intended

--- CW1/Main.c
+++ CW1/Main.c
@@ -2,7 +2,27 @@
 #include "Main.h"
 
 int main(int argc, const char * argv[]){
-   typedef OP<OP<OP<LIT<5>,PLUS,LIT<10> >, DIVIDE, OP<LIT<3>, PLUS, LIT<2> > >, MULTIPLY, VAR > FORMULA;
-   printf("%f\n", (FORMULA::eval(5)));
+   // (5+5) - 15
+   typedef SUBTRACT<PAREN<ADDITION<LIT<5>, LIT<5> > >,LIT<15>  > FORMULA;
+   // (x-2)^2
+   typedef EXPONENT<PAREN<SUBTRACT<VAR, LIT<2> > >, LIT<2> > FORMULA2;
+   // x^4 + (x-2)^2 + (x*3) + 5
+   typedef ADDITION<ADDITION<ADDITION<EXPONENT<VAR, LIT<4> >, EXPONENT<PAREN<SUBTRACT<VAR, LIT<2> > >, LIT<2> > >, PAREN<MULTIPLY<VAR, LIT<3> > > >, LIT<5> > FORMULA3;
+   // (5-2)^3
+   typedef EXPONENT<PAREN<SUBTRACT<LIT<5>, LIT<2> > >, LIT<3> > FORMULA4;
+   // x^(x + 10) - Be careful of integer overflow
+   typedef EXPONENT<VAR, PAREN<ADDITION<VAR, LIT<10> > > > FORMULA5;
+   // x^x --
+   typedef EXPONENT<VAR, VAR> FORMULA6;
+
+
+   // (10-5)^(x+2)
+   typedef EXPONENT<PAREN<SUBTRACT<LIT<10>, LIT<5> > >, PAREN<ADDITION<VAR, LIT<2> > > > FORMULA7;
+
+   printf("Result is %f\n", FORMULA3::eval(2));
+
+   double x = EVAL<2, FORMULA7>::RET;
+   printf("Result 2 is %f\n", x);
+
    return 0;
 }
--- CW1/Main.h
+++ CW1/Main.h
@@ -2,33 +2,86 @@
 #define _main_h_
 
 #include <functional>
+#include <math.h>
 
-#define PLUS std::plus<double>
-#define DIVIDE std::divides<double>
-#define SUBTRACT std::minus<double>
-#define MULTIPLY std::multiplies<double>
 
-// Data types
+// Dynamic evaluation
+#define INLINE_RET(BODY) \
+   static inline double eval(double d){ \
+      BODY \
+   };
+#define LR_TEMP template <class L, class R> struct 
+template <class EXP> struct PAREN { INLINE_RET( return EXP::eval(d); )};
+LR_TEMP EXPONENT { INLINE_RET( return pow(L::eval(d), R::eval(d)); )};
+LR_TEMP MULTIPLY { INLINE_RET( return L::eval(d) * R::eval(d); )};
+LR_TEMP DIVIDE   { INLINE_RET( return L::eval(d) / R::eval(d); )};
+LR_TEMP ADDITION { INLINE_RET( return L::eval(d) + R::eval(d); )};
+LR_TEMP SUBTRACT { INLINE_RET( return L::eval(d) - R::eval(d); )};
+struct VAR  { INLINE_RET( return d; )};
+template <int v> struct LIT { INLINE_RET( return v; ) };
+
+
+// Static evaluation 
 //
-struct VAR {
-   static inline double eval(double d){
-      return d;
-   };
+//
+//
+//
+
+// Loop for exponent expansion
+
+template <int exp, int count> struct EXPONENT_UNROLL{
+   enum { RET = exp * EXPONENT_UNROLL<exp, (count-1)>::RET};
 };
 
-template <int v> 
-struct LIT {
-   static inline double eval(double d){
-      return v;
-   };
+template <int exp> struct EXPONENT_UNROLL<exp, 1>{
+   enum { RET = 1 };
 };
 
-template <class L, typename Op,  class R>
-struct OP{
-   static inline double eval(double d){
-      Op o;
-      return o(L::eval(d), R::eval(d));
-   };
+// Main parser
+
+template <int d, class F>
+struct EVAL;
+
+template <int d, class EXP>
+struct EVAL<d, PAREN<EXP> > {
+   enum { RET = EVAL<d, EXP>::RET };
+};
+
+// This could maybe be improved - but it allows full expressions to be parenthesised as an exponent
+// i.e. x^(55+x)
+template <int d, class base, class exp>
+struct EVAL<d, EXPONENT<base, exp > > {
+   enum { RET = EVAL<d, base>::RET * EXPONENT_UNROLL<EVAL<d, base>::RET, EVAL<d, exp>::RET>::RET };
+};
+
+template <int d, class L, class R>
+struct EVAL<d, MULTIPLY<L,R> > {
+   enum { RET = EVAL<d, L>::RET * EVAL<d, R>::RET };
+};
+
+template <int d, class L, class R>
+struct EVAL<d, DIVIDE<L,R> > {
+   enum { RET = EVAL<d, L>::RET / EVAL<d, R>::RET };
+};
+
+template <int d, class L, class R>
+struct EVAL<d, ADDITION<L,R> > {
+   enum { RET = EVAL<d, L>::RET + EVAL<d, R>::RET };
+};
+
+template <int d, class L, class R>
+struct EVAL<d, SUBTRACT<L,R> > {
+   enum { RET = EVAL<d, L>::RET - EVAL<d, R>::RET };
+};
+
+template <int d>
+struct EVAL<d, VAR> {
+   enum { RET = d};
+};
+
+template <int d, int V>
+struct EVAL<d, LIT<V> > {
+   enum {RET = V};
 };
 
 #endif
